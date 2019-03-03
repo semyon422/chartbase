5a7e167fcf4827eb604501ed882efdf841ee9bea
semyon422 <semyon422@gmail.com>
2019-03-03 10:38:17 +0000

    Added Quaver hitsounds

--- quaver/NoteDataImporter.lua
+++ quaver/NoteDataImporter.lua
@@ -16,6 +16,13 @@ end
 
 NoteDataImporter.inputType = "key"
 
+NoteDataImporter.HitSounds = {
+	Normal = "sound-hit.wav",
+	Whistle = "sound-hitwhistle.wav",
+	Finish = "sound-hitfinish.wav",
+	Clap = "sound-hitclap.wav"
+}
+
 NoteDataImporter.init = function(self)
 	self.inputIndex = self.hitObject.Lane
 	self.startTime = self.hitObject.StartTime
@@ -24,6 +31,22 @@ NoteDataImporter.init = function(self)
 	local lastTime = self.endTime or self.startTime
 	if lastTime > self.noteChartImporter.totalLength then
 		self.noteChartImporter.totalLength = lastTime
+	end
+	
+	self.sounds = {}
+	self.hitSound = self.hitObject.HitSound
+	if not self.hitSound then
+		self.sounds[1] = self.HitSounds.Normal
+	else
+		local soundsTable = self.hitSound:split(",")
+		for _, sound in ipairs(soundsTable) do
+			sound = sound:trim()
+			self.sounds[#self.sounds + 1] = NoteDataImporter.HitSounds[sound] or sound
+		end
+	end
+	
+	for _, sound in ipairs(self.sounds) do
+		self.noteChart:addResource("sound", sound)
 	end
 end
 
