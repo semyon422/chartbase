b6683f331e798853be3bdd26ed32c9fac2bef352
semyon422 <semyon422@users.noreply.github.com>
2020-05-18 21:21:57 +0500

    Use another new line pattern

--- bms/NoteChartImporter.lua
+++ bms/NoteChartImporter.lua
@@ -30,7 +30,7 @@ NoteChartImporter.import = function(self)
 	if not self.bms then
 		self.bms = BMS:new()
 		self.bms.pms = self.path:sub(-4, -1) == ".pms"
-		self.bms:import(self.content:gsub("\r\n", "\n"))
+		self.bms:import(self.content:gsub("\r[\r\n]?", "\n"))
 	end
 	
 	self:setInputMode()
