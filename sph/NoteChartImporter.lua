f7d939ac67acd63e747ca574b732d73eb681ca54
semyon422 <semyon422@users.noreply.github.com>
2020-07-14 19:50:10 +0500

    Fix sph loading

--- sph/NoteChartImporter.lua
+++ sph/NoteChartImporter.lua
@@ -5,16 +5,19 @@ NoteChartImporter_metatable.__index = NoteChartImporter
 
 NoteChartImporter.new = function(self)
 	local noteChartImporter = {}
-	
+
 	setmetatable(noteChartImporter, NoteChartImporter_metatable)
-	
+
 	return noteChartImporter
 end
 
-NoteChartImporter.import = function(self, noteChartString)
+NoteChartImporter.import = function(self)
 	local directoryPath, fileName = self.path:match("^(.+)/(.-)%.sph$")
-	self.noteChart = dofile(directoryPath .. "/" .. fileName .. ".lua")(directoryPath)
-	return self.noteChart
+
+	local noteCharts = dofile(directoryPath .. "/" .. fileName .. ".lua")(directoryPath, self.path)
+
+	print(#noteCharts)
+	self.noteCharts = noteCharts
 end
 
 return NoteChartImporter
