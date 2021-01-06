f362b59cf59423592c9cef32045eb7fdc678e884
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-01-06 20:13:51 +0100

    Add comments to midi

--- midi/MID.lua
+++ midi/MID.lua
@@ -5,7 +5,7 @@ local MID = {}
 local MID_metatable = {}
 MID_metatable.__index = MID
 
-MID.new = function(self, midString, path)
+MID.new = function(self, midString)
 	local mid = {}
 
 	setmetatable(mid, MID_metatable)
@@ -14,18 +14,59 @@ MID.new = function(self, midString, path)
 	return mid
 end
 
-MID.process = function(self, midString, path)
-	local opus = MidiLua.midi2opus(midString)
+MID.process = function(self, midString)
+    -- opus format:
+    -- my_opus = {
+    --     96, -- MIDI-ticks per beat
+    --     {   -- first track
+    --         {'patch_change', 0, 1, 8},   -- events
+    --         {'set_tempo', 0, 750000},    -- microseconds per beat
+    --         -- 'note_on/off',  dtime, channel, note, velocity
+    --         {'note_on',   5, 1, 25, 96},
+    --         {'note_off', 96, 1, 25, 0},
+    --         {'note_on',   0, 1, 29, 96},
+    --         {'note_off', 96, 1, 29, 0},
+    --     },
+    --     {   -- second track
+    --         {'note_on',   5, 1, 25, 96},
+    --         {'note_off', 96, 1, 25, 0},
+    --     }
+    -- }
+    local opus = MidiLua.midi2opus(midString)
+    
+    -- score format:
+    -- my_score = {
+    --     96,
+    --     {
+    --         {'patch_change', 0, 1, 8},
+    --         -- 'note', start_time, duration, channel, pitch, velocity
+    --         {'note',   5, 96, 1, 25, 98},
+    --         {'note', 101, 96, 1, 29, 98},
+    --     },
+    -- }
+
+    -- to_millisecs just changes the opus to:
+    -- {
+    --     1000,
+    --     {
+    --         {'set_tempo', 1000000},
+    --         -- ...
+    --     },
+    --     {
+    --         {'set_tempo', 1000000},
+    --         -- ...
+    --     }
+    -- }
+    -- regardless of the set_tempo differences between the tracks
 	self.score = MidiLua.opus2score(MidiLua.to_millisecs(opus))
 
-	self.title = path:match("^.*/(.*).mid$")
-
+    -- calculate the bpm changes for the measure line
 	local bpm = {}
-	for _, event in ipairs(opus[2]) do
+	for _, event in ipairs(opus[2]) do -- opus instead of score, because to_millisecs sets all set_tempo changes to 1000000
 		if event[1] == "set_tempo" then
 			bpm[#bpm+1] = {
 				dt = event[2] / 1000,
-				bpm = math.floor((60000000 / event[3]) + 0.5)
+				bpm = math.floor((60000000 / event[3]) + 0.5) -- minute / microseconds
 			}
 		end
 	end
@@ -35,7 +76,7 @@ MID.process = function(self, midString, path)
 			dt = 0,
 			bpm = 60
 		}
-	elseif bpm[1]["dt"] ~= 0 then
+	elseif bpm[1]["dt"] ~= 0 then -- make sure the first bpm starts at 0
 		bpm[1]["dt"] = 0
 	end
 	self.bpm = bpm
