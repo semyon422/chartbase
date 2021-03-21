a8977b909be051d1d576f8d741d111397a674d60
semyon422 <semyon422@users.noreply.github.com>
2021-03-21 19:29:24 +0500

    Fix pms detection

--- bms/NoteChartImporter.lua
+++ bms/NoteChartImporter.lua
@@ -31,7 +31,7 @@ NoteChartImporter.import = function(self)
 	
 	if not self.bms then
 		self.bms = BMS:new()
-		self.bms.pms = self.path:sub(-4, -1) == ".pms"
+		self.bms.pms = self.path:lower():sub(-4, -1) == ".pms"
 		self.bms:import(self.content:gsub("\r[\r\n]?", "\n"))
 	end
 	
