92bb56fefe6d0ab261d8c901619ea5b995b09273
semyon422 <semyon422@users.noreply.github.com>
2023-07-03 16:19:33 +0500

    Fix encoding on ksh charts

--- notechart/EncodingConverter.lua
+++ notechart/EncodingConverter.lua
@@ -4,8 +4,8 @@ local utf8validate = require("utf8validate")
 local EncodingConverter = {}
 
 local Encodings = {
-	{"UTF-8", "ISO-8859-1"},
 	{"UTF-8", "SHIFT-JIS"},
+	{"UTF-8", "ISO-8859-1"},
 	{"UTF-8", "CP932"},
 	{"UTF-8", "EUC-KR"},
 	{"UTF-8", "US-ASCII"},
